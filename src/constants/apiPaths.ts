0181a213a1518d12db0aa9b9024ac7a1c40f1d8c
Mukhammadamin <mukhammadamin.sultonov@litmos.com>
2025-03-08 03:46:26 +0500

    fix frontend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://4wdy8ny0j0.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://yak67cpsbi.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://4wdy8ny0j0.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
