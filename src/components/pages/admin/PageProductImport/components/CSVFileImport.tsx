9ab0060c4f4220f17fd220d7734f089038822659
Mukhammadamin <mukhammadamin.sultonov@litmos.com>
2025-03-09 02:09:52 +0500

    fix front

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -24,30 +24,29 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   };
 
   const uploadFile = async () => {
+    if (!file) {
+      console.error("No file selected for upload.");
+      return;
+    }
+
     console.log("uploadFile to", url);
 
-    try {
-      if (!file) {
-        throw new Error("No file selected");
-      }
-      const response = await axios({
-        method: "GET",
-        url,
-        params: {
-          name: encodeURIComponent(file.name),
-        },
-      });
-      console.log("File to upload: ", file.name);
-      console.log("Uploading to: ", response.data);
-      const result = await fetch(response.data, {
-        method: "PUT",
-        body: file,
-      });
-      console.log("Result: ", result);
-      setFile(undefined);
-    } catch (error) {
-      console.error("There was an error uploading the file", error);
-    }
+    // Get the presigned URL
+    const response = await axios({
+      method: "GET",
+      url,
+      params: {
+        name: encodeURIComponent(file.name),
+      },
+    });
+    console.log("File to upload: ", file.name);
+    console.log("Uploading to: ", response.data);
+    const result = await fetch(response.data.url, {
+      method: "PUT",
+      body: file,
+    });
+    console.log("Result: ", result);
+    setFile(undefined);
   };
   return (
     <Box>
